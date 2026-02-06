b67b8ece1a74168e6dcd4def415042bdd4d393a8
Lee <lee@everpayapp.co.uk>
2026-02-06 00:25:42 +0000

    Fix public creator page params

--- src/app/creator/(public)/[username]/page.tsx
+++ src/app/creator/(public)/[username]/page.tsx
@@ -1,7 +1,6 @@
 import CreatorClient from "./CreatorClient";
 
 export default function Page({ params }: { params: { username: string } }) {
-  const decodedUsername = decodeURIComponent(params.username);
-  return <CreatorClient username={decodedUsername} />;
+  return <CreatorClient username={params.username} />;
 }
 
