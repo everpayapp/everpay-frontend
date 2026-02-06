f19574e1c3a0a08df6a1db27446fea15c568c26f
Lee <lee@everpayapp.co.uk>
2026-02-06 14:36:01 +0000

    Fix public creator params (no Promise/await)

--- src/app/creator/(public)/[username]/page.tsx
+++ src/app/creator/(public)/[username]/page.tsx
@@ -3,4 +3,3 @@ import CreatorClient from "./CreatorClient";
 export default function Page({ params }: { params: { username: string } }) {
   return <CreatorClient username={params.username} />;
 }
-
