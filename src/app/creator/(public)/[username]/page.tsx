6f2a02d4cc5a0b61019fc59cad1c8abd3c74c287
Lee <lee@everpayapp.co.uk>
2026-02-06 00:03:46 +0000

    Fix public creator route params (no more undefined username)

--- src/app/creator/(public)/[username]/page.tsx
+++ src/app/creator/(public)/[username]/page.tsx
@@ -1,7 +1,5 @@
 import CreatorClient from "./CreatorClient";
 
 export default function Page({ params }: { params: { username: string } }) {
-  const decodedUsername = decodeURIComponent(params.username);
-  return <CreatorClient username={decodedUsername} />;
+  return <CreatorClient username={params.username} />;
 }
-
